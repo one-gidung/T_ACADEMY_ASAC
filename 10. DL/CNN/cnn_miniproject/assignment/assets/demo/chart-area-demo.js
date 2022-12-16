f0830fdf99f1b2b8f4743a4c9c6244ad04fb354a
이승희 (Lee Seung-Hee) <dlel2000@naver.com>
2022-12-16 13:57:48 +0900

    1

---
+++ 10. DL/CNN/cnn_miniproject/assignment/assets/demo/chart-area-demo.js
@@ -0,0 +1,57 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["2", "6", "8", "10", "13", "16", "21", "28", "33", "45", "59", "102", "202", "302", "404", "1000"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [0.11502347418, 0.17840375587, 0.24178403756, 0.39201877934,
+             0.50469483568, 0.62441314554, 0.70187793427, 0.76995305164,
+             0.80046948357, 0.84741784038, 0.89201877934, 0.9014084507,
+             0.92253521127, 0.92957746479, 0.93427230047, 0.92488262911],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
